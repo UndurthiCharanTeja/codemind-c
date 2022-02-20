6faa351a4a9a5829cb072dc178327225801a2889
UndurthiCharanTeja <98147960+UndurthiCharanTeja@users.noreply.github.com>
2022-02-20 08:45:24 +0530

    #include <stdio.h> int main() {     char c;
        scanf("%c", &c);  
        
        // %d displays the integer value of a character
        // %c displays the actual character
        printf("%d",  c);
        
        return 0;
    }

---
+++ Find_ASCII_Value_of_a_Character.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {  
+    char c;
+    
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("%d",  c);
+    
+    return 0;
+}
